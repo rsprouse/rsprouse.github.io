09d97d92416f19864f1e8231cb653db543b92c48
rsprouse <ronald@berkeley.edu>
2025-11-07 23:15:00 +0000

    Update from https://github.com/rsprouse/feeling-responsive/commit/1b3052630483313bbe8860595cb78a59a4590662

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3227,7 +3227,7 @@ function FastClick(a,b){"use strict";function c(a,b){return function(){return a.
                       })
                       .appendTo(self.$wrap);
 
-        self.$img.attr('alt': '');
+        self.$img.attr('alt': self.images[newIndex].alt || '');
         // Hack for IE img onload event
         self.$img.attr('src', self.images[newIndex]);
         return self;
